56356fb5aed49f68a64b53e66eb4fd924b5c0759
Aleksandr Krasnov <genryxy.alexandr@yandex.ru>
2021-08-09 20:13:29 +0300

    Add script and action

---
+++ results/README.md
@@ -0,0 +1,9 @@
+The table below contains info about results of benchmarks for different
+versions for existed SDK API operations. The unit of measurement of values
+ is ms/op. 
+
+| Version | HelmAdd | HelmRemove | HelmReindex |
+|:---|---:|---:|---:|
+| 0.3 | 50.708 ± 0.115 | 6.858 ± 0.009 | 51.498 ± 0.165 |
+| 1.0.1 | 50.454 ± 0.111 | 8.015 ± 0.017 | 51.308 ± 0.173 |
+| 1.1.1 | 30.448 ± 0.152 | 5.342 ± 0.011 | 31.055 ± 0.096 |
---
+++ write-results.py
@@ -0,0 +1,34 @@
+from os import listdir, scandir
+from os.path import isfile, join
+
+PLUS_MINUS = '±'
+
+scan_path = 'results'
+dirs = [f.path for f in scandir(mypath) if f.is_dir()]
+# choose only path which last folder name consists of only dots and digits
+dirs = list(filter(lambda dir: dir.split('/')[-1].replace('.', '').isdecimal(), dirs))
+# write results for latest version
+dirs = sorted(dirs)
+path = dirs[-1]
+
+res = {}
+vers = path.split('/')[-1]
+files = [f for f in listdir(path) if isfile(join(path, f))]
+for file in files:
+    with open(join(path, file), 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            # extract result from file required line
+            if PLUS_MINUS in line and '.run' in line:
+                words = line.split()
+                idx = words.index(PLUS_MINUS)
+                res[file.split('-')[0]] = ''.join(words[idx-1:idx+2])
+res = dict(sorted(res.items()))
+vals = list(res.values())
+vals.insert(0, vers)
+line = ' | '.join(vals)
+# target line which would be added to readme
+line = f'\n| {line} |'
+
+with open(join(scan_path, 'README.md'), 'a') as f:
+    print(f.write(line))
